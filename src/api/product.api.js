e8292e2bf9a7cbbed1801fa0dc1a36cf3189a2b3
Bhuvaneshwari Chinnadurai <bhuvaneshwari.chinnadurai@deliveryhero.com>
2024-03-17 17:15:39 +0100

    add cors headers

--- src/api/product.api.js
+++ src/api/product.api.js
@@ -2,26 +2,6 @@ import axios from "axios";
 
 const BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8080/api/v1";
 
-export const getProducts = () => {
-  return axios.get(`${BASE_URL}/products`, {
-    headers: {
-      'Content-Type': 'application/json',
-      'Origin': 'https://initial-code.d3n8p32hxdybas.amplifyapp.com',
-      'Access-Control-Request-Method': 'GET',
-      'Access-Control-Request-Headers': 'Authorization, Content-Type',
-    },
-    withCredentials: true, // Include cookies in the request
-  });
-};
+export const getProducts = () => axios.get(`${BASE_URL}/products`);
 
-export const createOrder = (payload) => {
-  return axios.post(`${BASE_URL}/orders`, payload, {
-    headers: {
-      'Content-Type': 'application/json',
-      'Origin': 'https://initial-code.d3n8p32hxdybas.amplifyapp.com',
-      'Access-Control-Request-Method': 'POST',
-      'Access-Control-Request-Headers': 'Authorization, Content-Type',
-    },
-    withCredentials: true, // Include cookies in the request
-  });
-};
+export const createOrder = payload => axios.post(`${BASE_URL}/orders`, payload);
