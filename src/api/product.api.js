7043b32248e1240640a773e6994cc738f00be682
Bhuvaneshwari Chinnadurai <bhuvaneshwari.chinnadurai@deliveryhero.com>
2024-03-17 17:58:31 +0100

    fix config

--- src/api/product.api.js
+++ src/api/product.api.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 import getConfig from "next/config";
 
-
 const { publicRuntimeConfig } = getConfig();
+const BASE_URL = publicRuntimeConfig.REACT_APP_API_BASE_URL;
 
 export const getProducts = () => {
-  const BASE_URL = publicRuntimeConfig.REACT_APP_API_BASE_URL;
   console.log("BASE_URL:", BASE_URL);
   return axios.get(`${BASE_URL}/products`);
 }
