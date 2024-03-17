78a9149a5c296ee5c9b3df29902486e4df55ab4a
Bhuvaneshwari Chinnadurai <bhuvaneshwari.chinnadurai@deliveryhero.com>
2024-03-17 18:09:55 +0100

    fix

--- src/api/product.api.js
+++ src/api/product.api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const BASE_URL = "http://ec2-3-71-1-37.eu-central-1.compute.amazonaws.com:8080/api/v1";
 
 export const getProducts = () => {
   console.log("BASE_URL:", BASE_URL);
