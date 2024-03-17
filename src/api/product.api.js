2fcf79cd91b3702d5a1c79569198626aa0fa913b
Bhuvaneshwari Chinnadurai <bhuvaneshwari.chinnadurai@deliveryhero.com>
2024-03-17 17:32:29 +0100

    console log

--- src/api/product.api.js
+++ src/api/product.api.js
@@ -4,6 +4,9 @@ const BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8080/ap
 console.log("BASE_URL:", BASE_URL); // Log the BASE_URL
 
 
-export const getProducts = () => axios.get(`${BASE_URL}/products`);
+export const getProducts = () => {
+  console.log("BASE_URL:", BASE_URL); // Log the BASE_URL
+  axios.get(`${BASE_URL}/products`);
+}
 
 export const createOrder = payload => axios.post(`${BASE_URL}/orders`, payload);
