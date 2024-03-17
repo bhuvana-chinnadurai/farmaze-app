21ee0b3eadc7d2471afaae4bc5f95a6df1f70173
Bhuvaneshwari Chinnadurai <bhuvaneshwari.chinnadurai@deliveryhero.com>
2024-03-17 17:54:02 +0100

    fix config

--- src/next.config.js
+++ src/next.config.js
@@ -1,5 +1,8 @@
 // next.config.js
 module.exports = {
+  images: {
+    unoptimized: true,
+  },
   publicRuntimeConfig: {
     REACT_APP_API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8080/api/v1",
   },
