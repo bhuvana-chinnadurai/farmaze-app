c02978cefb31086e5df7faf067fa255dc68c4fe0
Bhuvaneshwari Chinnadurai <bhuvanaccs@gmail.com>
2024-03-20 18:37:25 +0100

    product

--- src/api/product.api.js
+++ src/api/product.api.js
@@ -6,7 +6,7 @@ export const getProducts = async () => {
   const BASE_URL = publicRuntimeConfig.REACT_APP_API_BASE_URL
   try {
     const response = await axios.get(`${BASE_URL}/products`);
-    return response;
+    return response.data;
   } catch (error) {
     console.error("Error fetching products:", error);
     return null;
@@ -16,7 +16,7 @@ export const getProducts = async () => {
 export const createOrder = async (payload) => {
   try {
     const response = await axios.post(`${BASE_URL}/orders`, payload);
-    return response;
+    return response.data;
   } catch (error) {
     console.error("Error creating order:", error);
     return null;
