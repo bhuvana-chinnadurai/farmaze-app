7b0673dc94579dfc26b4377b66e6630993645f44
Bhuvaneshwari Chinnadurai <bhuvanaccs@gmail.com>
2024-04-18 22:14:34 +0200

    fix auth

--- src/api/product.api.js
+++ src/api/product.api.js
@@ -5,7 +5,7 @@ import { publicRuntimeConfig } from 'next.config.js';
 export const getProducts = async () => {
   const BASE_URL = publicRuntimeConfig.REACT_APP_API_BASE_URL
   try {
-    const response = await axios.get(`${BASE_URL}/products`);
+    const response = await axios.get(`${BASE_URL}/products/`);
     console.log("response",response.data);
     return response.data
   } catch (error) {
@@ -21,7 +21,7 @@ export const createOrder = async (payload) => {
 
   try {
     console.log("token ",token)
-    const response = await axios.post(`${BASE_URL}/orders`, payload,{ 
+    const response = await axios.post(`${BASE_URL}/orders/`, payload,{ 
       headers: {
       'Authorization': `Bearer ${token}`, // Set the Authorization header
       'Content-Type': 'application/json'
