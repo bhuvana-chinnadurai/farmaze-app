3941ef9cc22574d7209ad4525583f94a798d573b
Bhuvaneshwari Chinnadurai <bhuvanaccs@gmail.com>
2024-04-18 18:27:37 +0200

    set token

--- src/api/product.api.js
+++ src/api/product.api.js
@@ -16,8 +16,15 @@ export const getProducts = async () => {
 
 export const createOrder = async (payload) => {
   const BASE_URL = publicRuntimeConfig.REACT_APP_API_BASE_URL
+  // Retrieve the token from sessionStorage or localStorage
+  const token = sessionStorage.getItem('token'); // or localStorage.getItem('token');
+
   try {
-    const response = await axios.post(`${BASE_URL}/orders`, payload);
+    const response = await axios.post(`${BASE_URL}/orders`, payload,{ 
+      headers: {
+      'Authorization': `Bearer ${token}`, // Set the Authorization header
+      'Content-Type': 'application/json'
+    }});
     return response.data;
   } catch (error) {
     console.error("Error creating order:", error);
